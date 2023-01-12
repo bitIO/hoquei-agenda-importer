4e2e3ccede4cd4526133175e1452a2cec4a0be64
Francisco Calle <francisco.calle.moreno@gmail.com>
2023-01-12 17:43:33 +0100

    fix: rename lambda and cron event

--- lib/hockey-events-importer-stack.ts
+++ lib/hockey-events-importer-stack.ts
@@ -12,27 +12,31 @@ export class HockeyEventsImporterStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambdaImporter = new NodejsFunction(this, 'EventsImporter', {
-      bundling: {
-        externalModules: ['aws-sdk'],
-        minify: true,
+    const lambdaImporter = new NodejsFunction(
+      this,
+      'HoqueiLambdaEventsImporter',
+      {
+        bundling: {
+          externalModules: ['aws-sdk'],
+          minify: true,
+        },
+        entry: join(__dirname, '../lambdas/importer.ts'),
+        environment: {
+          CLIENT: 'fmp',
+          IDM: '1',
+          TEMP_ID: '21',
+          URL: 'https://ns3104249.ip-54-37-85.eu/shared/portales_files/agenda_portales.php',
+        },
+        functionName: 'hoquei-events-importer',
+        logRetention: RetentionDays.THREE_DAYS,
+        runtime: Runtime.NODEJS_18_X,
+        timeout: Duration.seconds(60),
       },
-      entry: join(__dirname, '../lambdas/importer.ts'),
-      environment: {
-        CLIENT: 'fmp',
-        IDM: '1',
-        TEMP_ID: '21',
-        URL: 'https://ns3104249.ip-54-37-85.eu/shared/portales_files/agenda_portales.php',
-      },
-      functionName: 'events-importer',
-      logRetention: RetentionDays.THREE_DAYS,
-      runtime: Runtime.NODEJS_18_X,
-      timeout: Duration.seconds(60),
-    });
+    );
 
-    const rule = new Rule(this, 'Rule', {
+    const rule = new Rule(this, 'HoqueiImporterCronRule', {
       description: 'Launches a lambda to import hockey matches',
-      ruleName: 'hockeyMatchesImportCron',
+      ruleName: 'hoqueiImporterCron',
       schedule: Schedule.expression('cron(0 18 ? * 3 *)'),
     });
     rule.addTarget(new LambdaFunction(lambdaImporter));
